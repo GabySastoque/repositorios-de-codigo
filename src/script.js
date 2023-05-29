41ff9a58fae0de7c0ab58c82061c491d46e17d58
GabySastoque <sastoque98hotmail.com>
2023-05-28 22:44:52 -0500

    Añade boton

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
 const boton_1 = document.getElementById('cambiarColor_1')
+const boton_2 = document.getElementById('cambiarColor_2')
 
+boton_2.addEventListener('click', () => {
+  const body = document.querySelector('body')
+  body.style.backgroundColor = 'blue'
+})
 boton_1.addEventListener('click', () => {
   const body = document.querySelector('body')
   body.style.backgroundColor = 'yellow'
